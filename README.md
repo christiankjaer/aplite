c5c274d8b94ad2bad56459449426369a528fa7a4
Christian Kjær Larsen <christianden@gmail.com>
2016-04-24 12:52:19 +0200

    Added readme

---
+++ README.md
@@ -0,0 +1,18 @@
+APLite
+======
+
+Compile with `ghc -o aplite Repl.hs`
+
+Calculating factorials
+```
+> + / iota 10
+3628800
+```
+
+Matrices are supported
+```
+> 3 3 # 3 * iota 3*3
+3   6   9
+12  15  18
+21  24  27
+```
--- Repl.hs
+++ Repl.hs
@@ -1,8 +1,9 @@
-module Repl where
+module Main where
 import qualified EvalApl as E
 import qualified ParseApl as P
 
 main = do
+    putStrLn ""
     putStr "> "
     line <- getLine
     putStrLn $ show $ E.eval $ P.parseString line
