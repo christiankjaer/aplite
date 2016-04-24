4edbb706f6c8adda6def605651dec1d5e30f9a77
Christian Kjær Larsen <christianden@gmail.com>
2016-04-24 17:26:10 +0200

    Should be main to compile

--- Repl.hs
+++ Repl.hs
@@ -1,9 +1,9 @@
-module Repl where
+module Main where
 import qualified EvalApl as E
 import qualified ParseApl as P
 
-repl = do
+main = do
     putStr "> "
     line <- getLine
     (putStrLn . show . E.eval . P.parseString) line
-    repl
+    main
